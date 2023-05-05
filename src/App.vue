29c60ab8b7cbd3c72b1e17ab46f361081b35ca79
Ebraim30 <ebraim.herrera@gmail.com>
2023-05-05 15:56:53 -0300

    Altera npm

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="formulario">
+        <form action="submitForm">
+            <div class="input-name">
+                <label for="name">Nome</label>
+                <input type="text" placeholder="Digite o nome"></div>
+                <div class="input-email">
+                <label for="email">Email:</label>
+                <input type="email" placeholder="Digite seu email"></div>
+                <div class="input-pass">
+                <label for="password">Senha:</label>
+                <input type="password" placeholder="Digite sua senha"></div>
+                <button type="submit">Enviar informações</button>
+        </form>
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
