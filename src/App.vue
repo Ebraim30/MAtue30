e1b264f2c7450ad91bd039d0e77ac182b4c1f5f5
Ebraim30 <ebraim.herrera@gmail.com>
2023-05-07 21:18:12 -0300

    Feito-Estado-Cidade-Hobbie-Liguagem-etc...

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,46 @@
                 <div class="input-string">
                 <label for="string">Cidade</label>  
                 <input type="string" placeholder="Digite sua cidade"></div>
+                <label for="string">Estado</label> 
+                <select v-model="estado">
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espírito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option></select>
+                <div class="input-string">
+                <label for="string">Hobbie</label>  
+                <input type="string" placeholder="Digite seu Hobbie"></div>
+                <label for="string">Linguagem</label> 
+                <select v-model="lingua">
+                <option value="JavaScript">JavaScript</option>
+                <option value="C++">C++</option>
+                <option value="Python">Python</option>
+                <option value="Java">Java</option>
+                <option value="C#">C#</option>
+                </select>
                 <button type="submit">Enviar informações</button>
         </form>
     </div>
